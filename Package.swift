6284108da389a2692bc313c89e99f457f2ff1a85
Emircan Aydın <emircanaydin973@gmail.com>
2022-10-19 15:51:20 +0300

    min target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkEntityLayer",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS("10.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
